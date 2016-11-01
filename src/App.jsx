6ae5ae9e4b919a401d730b5ab951f864bbc94708
cecstar <yocecia@gmail.com>
2016-11-01 01:08:31 +0000

    files split into component modules and tested in the DOM

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import React, {Component} from 'react';
+import ChatBar from './ChatBar.jsx';
+import Message from './Message.jsx';
+import MessageList from './MessageList.jsx';
+
 
 class App extends Component {
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <div className="wrapper">
+        <nav>
+          <h1>Chatty</h1>
+        </nav>
+        <div id="message-list">
+          <MessageList />
+          <Message />
+        </div>
+      <ChatBar />
+    </div>
     );
   }
 }
---
+++ src/Message.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class Message extends Component {
+  render() {
+    return (
+      <div className="message system">
+        Anonymous1 changed their name to nomnom. OR DID THEY???
+      </div>
+    );
+  }
+}
+export default Message;
