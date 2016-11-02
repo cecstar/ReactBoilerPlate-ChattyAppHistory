1f635128ee620f324584f4f8657a89d3a93062b7
cecstar <yocecia@gmail.com>
2016-11-02 01:44:19 +0000

    functional chatapp, pre-refactor to use refs

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import MessageList from './MessageList.jsx';
 class App extends Component {
   constructor(props) {
     super(props);
+    this.postNewMessage = this.postNewMessage.bind(this)
 
     this.state = {
       currentUser: {name: "Bob"},
@@ -22,19 +23,26 @@ class App extends Component {
     };
   }
 
+postNewMessage(name, content){
+const newMessage = {id:3, username: name, content: content}
+const post = this.state.messages.concat(newMessage)
+this.setState({messages: post})
+}
   render() {
     console.log(this.state)
+    console.log(this.state.messages)
     return (
       <div className="wrapper">
         <nav>
-          <h1>Chatty</h1>
+          <h1>Chatty Cathy</h1>
         </nav>
         <div id="message-list">
           <MessageList data={this.state.messages}/>
         </div>
-      <ChatBar currentUser={this.state.currentUser}/>
+      <ChatBar currentUser={this.state.currentUser} newMessage={this.postNewMessage}/>
     </div>
     );
   }
 }
+
 export default App;
--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -4,20 +4,48 @@ import MessageList from './MessageList.jsx';
 import App from './App.jsx';
 
 class ChatBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: '', username: ''};
+    this.handleChangeUsername = this.handleChangeUsername.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleChangeUsername(event) {
+    this.setState({username: event.target.value});
+  }
+
+  handleSubmit(event) {
+    if (event.key === 'Enter')
+    this.props.newMessage(this.state.username, this.state.value)
+  }
+
   render() {
     return (
+      <div>
       <footer>
         <input
           id="username"
           type="text"
           placeholder={this.props.currentUser.name}
+          onChange={this.handleChangeUsername}
           />
         <input
           id="new-message"
           type="text"
           placeholder="Type a message and hit ENTER"
+          value={this.state.value}
+          onChange={this.handleChange}
+          onKeyDown={this.handleSubmit}
+          onClick={this.handleSubmit}
           />
       </footer>
+      </div>
     );
   }
 }
